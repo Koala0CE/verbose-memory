c53007be7e21e77c523588330db255a6aa78a262
Cristina Giuroiu <cristina.giuroiu@plasmics.com>
2023-11-04 22:44:29 +0100

    removed unused

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,12 @@ import styles from "./page.module.css";
 import { Hero } from "./_components/Hero";
 import { Stack } from "@mui/material";
 import CustomizedTimeline from "./_components/Timeline";
-import TitlebarBelowImageList from "./album/page";
 
 export default function Home() {
   return (
     <Stack spacing={5} mt={5} justifyContent={"center"} alignItems={"center"}>
       <Hero />
-      {/* <TimeLine /> */}
       <CustomizedTimeline />
-      <TitlebarBelowImageList />
     </Stack>
   );
 }
