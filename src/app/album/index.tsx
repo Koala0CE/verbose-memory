6c61108f02e20f839b279a187e72b032bf625041
Cristina Giuroiu <cristina.giuroiu@plasmics.com>
2023-11-04 22:13:42 +0100

    album

---
+++ src/app/album/index.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+
+export default function TitlebarBelowImageList() {
+  return (
+    <ImageList sx={{ width: 500, height: 450 }}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            alt={item.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={item.title}
+            subtitle={<span>at: {item.location}</span>}
+            position="below"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
+
+const itemData = [
+  {
+    img: "/valentinesDay.png",
+    title: "Valentine's Day Flowers",
+    location: "@Preston Home",
+  },
+];
