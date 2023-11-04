07ca3cdaa3e436a3aa4afb52933aa10da10b6bce
Cristina Giuroiu <cristina.giuroiu@plasmics.com>
2023-11-04 21:42:46 +0100

    hero component

---
+++ src/app/_components/Hero.tsx
@@ -0,0 +1,10 @@
+import { Stack, Typography } from "@mui/material";
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <Stack>
+      <Typography variant="h1">Adventures of Jupi & Elvy</Typography>
+    </Stack>
+  );
+};
