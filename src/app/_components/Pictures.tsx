c793272bf3530683c6a3d00f61e065cc40b54be1
Cristina Giuroiu <cristina.giuroiu@plasmics.com>
2023-11-12 23:04:21 +0100

    save commit

--- src/app/_components/Pictures.tsx
+++ src/app/_components/Pictures.tsx
@@ -29,8 +29,10 @@ export const Pictures = ({
         {imageLoadError ? (
           <>
             <Image
-              width={500}
-              height={500}
+              //   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"#
+              fill
+              //   width={500}
+              //   height={500}
               src={fallbackImageUrl}
               alt={fallbackImageAlt}
             />
