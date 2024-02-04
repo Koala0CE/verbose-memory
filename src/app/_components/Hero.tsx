866ed3fe177b39bf66638c287e149f8683c047ef
Cristina Giuroiu <cristina.giuroiu@plasmics.com>
2024-02-04 18:31:53 +0100

    Add user role check in Hero component

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,7 +1,18 @@
+import { useUser } from "@auth0/nextjs-auth0/client";
 import { Stack, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { checkUserRole } from "../lib/roleChecker";
 
 export const Hero = () => {
+  const { user } = useUser();
+  const [hasRole, setHasRole] = useState(false);
+
+  // checks if the user has the "Friends & Family" role
+  useEffect(() => {
+    const hasFriendsAndFamilyRole = checkUserRole(user);
+    setHasRole(hasFriendsAndFamilyRole);
+  }, [user]);
+
   return (
     <Stack>
       <Typography
@@ -9,7 +20,7 @@ export const Hero = () => {
         fontWeight={500}
         fontSize={{ xs: "1.5rem", md: "5rem" }}
       >
-        Adventures of Jupi & Elvy
+        {hasRole ? "Adventures of Jupi & Elvy" : "Adventures of Jupi & Elvy"}
       </Typography>
     </Stack>
   );
