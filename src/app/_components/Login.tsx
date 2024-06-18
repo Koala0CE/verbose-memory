c7ea2fb0be374048d3f9884f008cf5d3fbfe4970
Koala0CE <reactor.koala0c@icloud.com>
2024-06-18 18:08:01 +0200

    Refactor Login component to use Material-UI Button for login functionality

--- src/app/_components/Login.tsx
+++ src/app/_components/Login.tsx
@@ -1,3 +1,19 @@
+import Button from "@mui/material/Button";
+import LoginIcon from "@mui/icons-material/Login";
+
 export default function Login() {
-  return <a href="/api/auth/login">Login</a>;
+  return (
+    <Button
+      variant="text"
+      sx={{
+        color: "white",
+        textTransform: "none",
+        p: 0,
+      }}
+      href="/api/auth/login"
+      startIcon={<LoginIcon />}
+    >
+      Login
+    </Button>
+  );
 }
